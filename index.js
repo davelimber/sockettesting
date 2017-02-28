0826b099719f0f3f4a41f52766cd88105555dff2
davelimber <dave.limber@gmail.com>
2017-02-28 08:31:04 -0700

    edits - index javascript to bottom!

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
 // app.get('/', function(req, res){
 //   res.send('<h1>Hello world</h1>');
 // });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/', function(req, res){
+//   res.sendfile('index.html');
+// });
+
 
 
 // io.on('connection', function(socket){
@@ -18,16 +41,21 @@ app.get('/', function (req, res) {
 //   });
 // });
 
-io.on('connection', function (socket) {
-    console.log('before chat message')
-    socket.broadcast.emit('hi');
-    socket.on('emessage', function (msg) {
-        console.log('hello!!')
-        console.log('message: ' + msg);
-    });
-});
+// io.on('connection', function (socket) {
+//     console.log('before chat message')
+//     // socket.broadcast.emit('hi');
+//     socket.on('emessage', function (msg) {
+//         console.log('hello!!')
+//         console.log('message: ' + msg);
+//     });
+// });
 
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
 
-http.listen(3001, function () {
-    console.log('listening on *:3001');
-});
+// http.listen(3001, function () {
+//     console.log('listening on *:3001');
+// });
